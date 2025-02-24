073fc3587010f7df229e7ae654ddfd361a4d280d
Hiren Chandra <hiren@akijfood.com>
2025-02-24 11:59:00 +0600

    2nd

---
+++ src/components/About/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>In 1970 Estabilist our Company. Began the company with 3 branches</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>Call me at night</p>
+    </div>
+  );
+};
+
+export default Contact;
